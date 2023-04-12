4c2e90d76785e87fc77eb0ee6fce900a25ede6d7
Gregory Sosa <gregory.sosa@gmail.com>
2023-04-11 21:47:57 -0400

    Feat: add some funcs

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -14,11 +16,23 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create a new snippet..."))
+	if r.Method == http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed yet...", 405)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header()["X-XSS-Protection"] = []string{"1; mode=block"}
+	w.Write([]byte(`{"result":"Create a new snippet..."}`))
 }
 
 func users(w http.ResponseWriter, r *http.Request) {
