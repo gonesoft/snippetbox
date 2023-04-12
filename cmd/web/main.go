cd9ed316fa8c68e17e3179e726e4cdd6e6aad37f
Gregory Sosa <gregory.sosa@gmail.com>
2023-04-12 01:36:02 -0400

    fix: add database layer -Adding postgres config and initialization

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	addr := flag.String("addr", ":8080", "HTTP network address")
+
+	dns := flag.String("dns", "postgresql://postgres:password@localhost/graphql?sslmode=disable", "Postgres datasource name")
+
 	flag.Parse()
 
 	infolog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dns)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -26,7 +38,20 @@ func main() {
 	}
 
 	infolog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 
+}
+
+func openDB(dns string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dns)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
