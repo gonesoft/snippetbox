bfd6f53f5c0e8fd8fcad8bf325bfc5df273c365e
Gregory Sosa <gregory.sosa@gmail.com>
2023-04-11 22:25:55 -0400

    fix: add refactor to build folder scturcture add template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -37,16 +36,4 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 
 func users(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a specific user..."))
-}
-
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-
-	log.Println("Starting server on :8080")
-	err := http.ListenAndServe(":8080", mux)
-	log.Fatal(err)
-
 }
